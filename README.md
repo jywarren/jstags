702dc78ebc9fefacb47c4453594584bb50183c1e
jywarren <jeff@unterbahn.com>
2017-01-27 20:06:14 -0500

    unique ids

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+A form generator following conventions in [WordPress short codes](https://github.com/MWDelaney/bootstrap-3-shortcodes)
+
+Goals:
+
+* make it return the original markup
+* make it return before/after change
+* make a submit() method to send it somewhere
+* make an "onComplete" feedback - green/red checkmark/x
+* 
+
+
---
+++ dist/short-code-forms.js
@@ -0,0 +1,52 @@
+
+function replaceFormTag(match, p1, p2, p3) {
+
+  var o = '',
+      contents = p3 || p2;
+      uniqueId = p3 ? p2 : undefined;
+  o += '<form';
+  if (uniqueId) o += ' id="' + uniqueId + '"';
+  o += ' class="well">';
+
+  var innerTag = contents.replace(/\[(\w+):*(\w+)*\]/, function replaceInnerTag(m, pp1, pp2) {
+
+    var placeholder = pp2 || "";
+    var input = '';
+    input += '<p><input class="form-control" type="text" placeholder="' + placeholder + '" /></p>';
+    return input;
+
+  });
+
+  o += innerTag;
+  var submit = p1 || "Add";
+  o += '<p><button class="btn btn-default" type="submit">' + submit  + '</button></p>\n</form>';
+
+  return o;
+
+}
+
+function shortCodeForm(el) {
+  var regex = /\[form:*(\w+)*:*(\w+)*\](\s*\[[^\/].*\]\s*)*\[\/form\]/g;
+  var output = el.innerHTML.replace(regex, replaceFormTag)
+  return output;
+}
+
+function replacePrompt(match, p1, p2) {
+
+  var o = '',
+      placeholder = p2 || "",
+      submit = p1 || "Add";
+  o += '<form id="UNIQUE" class="well">';
+  o += '<p><input class="form-control" type="text" placeholder="' + placeholder + '" /></p>';
+  o += '<p><button class="btn btn-default" type="submit">' + submit  + '</button></p>\n</form>';
+
+  return o;
+
+}
+
+function shortCodePrompt(el) {
+  var regex = /\[prompt:(\w*):*(\w*)\]/g;
+  var output = el.innerHTML.replace(regex, replacePrompt);
+  return output;
+}
+
